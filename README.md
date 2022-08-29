7cc5bb6c2f7563fdc4f94f4812266dc0de6e130a
Subadra Poshitha <scienceposhitha@gmail.com>
2022-08-29 22:48:57 +0530

    Ai_Dark_Ezio is (Published) ✔

---
+++ README.md
@@ -0,0 +1,26 @@
+# Whats-Bot-MD_V2
+
+<p align="center">
+    <img src="https://raw.githubusercontent.com/AiDarkEzio/Whats-Bot/master/GojoMedia/D_E-DPC.jpg" alt="ADE" width="350" high="350" border="5"/>
+</p>
+
+- WhatsApp Using Lib Baileys Multi Device Private Bot.
+
+## Languages
+
+1. [`Node-Js`](https://nodejs.org/)
+2. [`Type-Stript`](https://www.typescriptlang.org/)
+3. [`Python`](https://www.python.org/)
+
+## Based Repo
+
+- [`Killua-Zoldyck`](https://github.com/zhwzein/Killua-Zoldyck "Base Bot")
+
+## Thanks To
+
+- [`Wa-Web`](https://github.com/adiwajshing/baileys)
+- [`Alien-Alfa`](https://github.com/Alien-Alfa)
+- [`DGXeon`](https://github.com/DGXeon)
+- [`AflahXrd`](https://github.com/nexusNw)
+
+### License: [MIT]2022
---
+++ config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  profile: {
+    ownerName: "", // AiDarkEzio
+    ownerNumb: "", // 940000000
+    botName: "", // Whats Bot MD
+  },
+  reply: {
+    notFound: "", // defult: "This Command not created. it was creating",
+    success: "", // defult: "Done ✓",
+    admin: "", // defult: "This Feature Is Only For Admin!",
+    botAdmin: "", // defult: "Bot Must Be Admin First!",
+    owner: "", // defult: "This Feature Is Only For Owner!",
+    group: "", // defult: "Feature Used Only For Groups!",
+    private: "", // defult: "Features Used Only For Private Chat!",
+    bot: "", // defult: "This Feature Is Only For Bot",
+    wait: "", // defult: "In process...",
+    linkm: "", // defult: "Where is the link?",
+    error: "", // defult: "Error!!",
+    endLimit: "", // defult: "Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours",
+    ban: "", // defult: "You have been banned by the owner, if you want to be unbanned, chat owner.",
+    nsfw: "", // defult: "The nsfw feature has not been activated, please contact the admin to activate",
+    banChat: "", // defult: "The bot was banned in this group, please contact the owner to unban",
+  },
+  setting: {
+    blockchat: [], // Your Jids
+  },
+};
